27d25421005c8ab30189b21a7746c15f8978ac0c
rafliyulistiawan <rafliyulistiawan@gmail.com>
2022-11-29 20:51:25 +0700

    add api for transaction

--- api/order.php
+++ api/order.php
@@ -35,3 +35,44 @@ function add_order()
 }
 
 // ADD TO TRANSACTION
+function add_transaction()
+{
+  global $connection;
+
+  $check = [
+    'id_order' => '',
+    'id_menu' => '',
+    'qty' => '',
+  ];
+  $req_body = json_decode(file_get_contents('php://input'), true);
+  $check_match = count(array_intersect_key($req_body, $check));
+
+  if ($check_match == count($check)) {
+    $id_order = $req_body['id_order'];
+    $id_menu = $req_body['id_menu'];
+    $qty = $req_body['qty'];
+
+    $command = "INSERT INTO transaksi 
+                VALUE ( '', '$id_order', '$id_menu', '$qty' )";
+    $query = mysqli_query($connection, $command);
+
+    if ($query) {
+      $response = [
+        'status' => 1,
+        'message' => 'Insert Success',
+      ];
+    } else {
+      $response = [
+        'status' => 0,
+        'message' => 'Error: ' . mysqli_error($connection)
+      ];
+    }
+  } else {
+    $response = [
+      'status' => 0,
+      'message' => 'Wrong Parameter'
+    ];
+  }
+  header('Content-Type: application/json');
+  echo json_encode($response);
+}
